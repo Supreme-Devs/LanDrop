6ded54327dad2038433c43fe369f017ce32aa807
LG-Crusty <pandittheroyal@gmail.com>
2025-09-07 19:13:02 +0530

    working on webrtc class logic

--- apps/web/src/services/webRTC.server.ts
+++ apps/web/src/services/webRTC.server.ts
@@ -3,33 +3,42 @@ import { messangerService } from "@/utils/signalChannel";
 
 // creating the server class
 export class webRTCPeer {
-  private connection : RTCPeerConnection
+  private connection: RTCPeerConnection;
 
   constructor(configuration: object) {
-   this.connection = new RTCPeerConnection(configuration)
+    this.connection = new RTCPeerConnection(configuration);
   }
 
   async makeCall() {
-    const pingingServer:{message: string} = messangerService.connect();
-    if (pingingServer) {
-      const message = {answer:string : }
-      const remoteDesc = new RTCSessionDescription(pingingServer)
-    } 
-    
-    // signalingChannel.addEventListener('message', async (message: {answer:}) => {
-    //     if (message.answer) {
-    //         const remoteDesc = new RTCSessionDescription(message.answer);
-    //         await peerConnection.setRemoteDescription(remoteDesc);
-    //     }
-    // });
-    const offer = await this.connection.createOffer();
-    await this.connection.setLocalDescription(offer);
-    messangerService.shareVal(offer);
+    const pingingServer: { message: string } = messangerService.connect();
+    if ((pingingServer && pingingServer.message != "")) {
+      // setting up the ice candidate
+      this.connection.onicecandidate = (event) => {
+        if (event.candidate !== null) {
+          // sending this to the signaling server
+          messangerService.shareVal(event.candidate);
+        } else {
+          // for safe case scenario
+          setTimeout(() => {
+            this.connection.close();
+          }, 3000);
+        }
+
+        //  creating offer for the server
+        const func = async () => {
+          const offer = await this.connection.createOffer();
+          await this.connection.setLocalDescription(offer);
+        };
+        func();
+      };
+    }
   }
 }
+
 const configuration = {
   iceServers: [{ urls: "stun:stun.l.google.com:19302" }],
 };
+
 const webRTCService = new webRTCPeer(configuration);
 
 export { webRTCService };
